c4620521db50816c229fb3bd675b7de9a5a65ee3
agustempo <agusvilas@gmail.com>
2019-08-04 23:14:47 -0300

    Primer commit y deployment

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'ciudades');
+
+// Project repository
+set('repository', 'git@github.com:techo/ciudades.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('159.89.145.87')
+    ->user('deployer')
+    ->identityFile('~/.ssh/deployerkey')
+    ->set('deploy_path', '/var/www/html/ciudades');
+
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+//before('deploy:symlink', 'artisan:migrate');
+
